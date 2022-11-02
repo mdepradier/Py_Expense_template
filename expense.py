4b84e6f5cffac50d4cd315b936141525754742f9
Maxime de Pradier d'Agrain <maxime.de-pradier-d-agrain@epita.fr>
2022-11-02 21:12:30 +0100

    Can a chose a spender among the users

--- expense.py
+++ expense.py
@@ -2,40 +2,48 @@ import csv
 
 from PyInquirer import prompt
 
+from user import get_user_list
+
 expense_questions = [
     {
-        "type":"input",
-        "name":"amount",
-        "message":"New Expense - Amount: ",
+        "type": "input",
+        "name": "amount",
+        "message": "New Expense - Amount: ",
     },
     {
-        "type":"input",
-        "name":"label",
-        "message":"New Expense - Label: ",
+        "type": "input",
+        "name": "label",
+        "message": "New Expense - Label: ",
     },
-    {
-        "type":"input",
-        "name":"spender",
-        "message":"New Expense - Spender: ",
-    },
-
 ]
 
+
+def chose_a_spender():
+    users = get_user_list()
+    user_choice = {
+        "type": "list",
+        "name": "chosen_user",
+        "message": "Chose a spender:",
+        "choices": users
+    }
+    option = prompt(user_choice)
+    return option['chosen_user']
+
+
 expense_report_file = "expense_report.csv"
+
+
 def write_expense_in_expense_report(amount, label, spender):
     with open(expense_report_file, "a") as f:
         csv.writer(f, delimiter=",").writerow([amount, label, spender])
 
 
-
 def new_expense(*args):
     infos = prompt(expense_questions)
+    spender = chose_a_spender()
     amount = infos['amount']
     label = infos['label']
-    spender = infos['spender']
     write_expense_in_expense_report(amount, label, spender)
     # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
     print("Expense Added !")
     return True
-
-
