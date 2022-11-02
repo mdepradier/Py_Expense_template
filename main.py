17788ac94da03cfa05b4165c270871a448a5c81a
Maxime de Pradier d'Agrain <maxime.de-pradier-d-agrain@epita.fr>
2022-11-02 20:57:26 +0100

    Can create a user

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from PyInquirer import prompt
 from examples import custom_style_2
 from expense import expense_questions,new_expense
+from user import add_user
+
 
 def ask_option():
     main_option = {
@@ -13,6 +15,13 @@ def ask_option():
     if (option['main_options']) == "New Expense":
         new_expense()
         ask_option()
+    elif (option['main_options']) == "Show Status":
+        print("Not implemented yet")
+
+    elif(option['main_options']) == "New User":
+        add_user()
+        ask_option()
+
 
 def main():
     ask_option()
--- user.py
+++ user.py
@@ -1,8 +1,54 @@
-from PyInquirer import prompt
-user_questions = [
+import csv
 
+from PyInquirer import prompt
+
+user_questions = [
+    {
+        "type": "input",
+        "name": "Name",
+        "message": "Name: ",
+    },
 ]
 
+user_csv_file = "users.csv"
+
+
+def add_user_to_csv_file(name):
+    with open(user_csv_file, "a") as f:
+        csv.writer(f, delimiter=",").writerow([name])
+
+
+# Read the csv file and return a list of users
+def get_user_list():
+    with open(user_csv_file, "r") as f:
+        reader = csv.reader(f, delimiter=",")
+        user_list = [row[0] for row in reader]
+    return user_list
+
+
+def verify_user_name(name):
+    if name == "":
+        print("Name cannot be empty")
+        return False
+
+    if not(name.isalpha()):
+        print("Name must contain only letters")
+        return False
+
+    if name in get_user_list():
+        print("Name already taken")
+        return False
+    return True
+
+
 def add_user():
-    # This function should create a new user, asking for its name
-    return
+    infos = prompt(user_questions)
+
+    name = infos['Name']
+    if not verify_user_name(name):
+        return False
+
+    add_user_to_csv_file(name)
+
+    print("User Added !")
+    return True
