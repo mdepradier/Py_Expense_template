51cadcdc432e99d732d6f77d08cb127b794660a0
Maxime de Pradier d'Agrain <maxime.de-pradier-d-agrain@epita.fr>
2022-11-02 20:42:21 +0100

    Expense can be written in a csv format

--- expense.py
+++ expense.py
@@ -1,3 +1,5 @@
+import csv
+
 from PyInquirer import prompt
 
 expense_questions = [
@@ -19,10 +21,19 @@ expense_questions = [
 
 ]
 
+expense_report_file = "expense_report.csv"
+def write_expense_in_expense_report(amount, label, spender):
+    with open(expense_report_file, "a") as f:
+        csv.writer(f, delimiter=",").writerow([amount, label, spender])
+
 
 
 def new_expense(*args):
     infos = prompt(expense_questions)
+    amount = infos['amount']
+    label = infos['label']
+    spender = infos['spender']
+    write_expense_in_expense_report(amount, label, spender)
     # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
     print("Expense Added !")
     return True
